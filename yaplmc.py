407685c85b7347768a14dda7cc1973909c87a9ce
Matthew Joyce <matsjoyce@gmail.com>
2015-03-14 18:14:56 +0000

    Merge yaplmc files

--- yaplmc.py
+++ yaplmc.py
@@ -20,6 +20,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import assembler
 import runner
 
+import tkinter
+from tkinter import (filedialog as fdialog, scrolledtext as stext,
+                     simpledialog, font as tkfont)
+import codemode
+import runmode
+
 __author__ = "Matthew Joyce"
 __copyright__ = "Copyright 2013"
 __credits__ = ["Matthew Joyce"]
@@ -29,35 +35,65 @@ __maintainer__ = "Matthew Joyce"
 __email__ = "matsjoyce@gmail.com"
 __status__ = "Development"
 
-if __name__ == "__main__":
-    import argparse
 
-    arg_parser = argparse.ArgumentParser(description=__doc__.split("\n")[1],
-                                         epilog="""
-yaplmc Copyright (C) 2014  Matthew Joyce
-This program comes with ABSOLUTELY NO WARRANTY.
-This is free software, and you are welcome to redistribute it
-under certain conditions. Type `yaplmc --licence` for details.
-                                         """.strip())
-    arg_parser.add_argument("-d", "--debug", help="debug level"
-                            " (repeat for more info, 2 is the max)",
-                            action="count", default=0)
-    arg_parser.add_argument("-f", "--file", help="lmc file",
-                            default=None)
-    arg_parser.add_argument("-l", "--licence", help="display licence",
-                            action="store_true")
-    arg_parser.add_argument("-V", "--version", help="display version",
-                            action="store_true")
+class GUIManager(tkinter.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("yaplmc")
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
 
-    args_from_parser = arg_parser.parse_args()
+        self.menu = tkinter.Menu(self)
+        self["menu"] = self.menu
 
-    if args_from_parser.licence:
-        print(__doc__.strip())
-        exit()
-    elif args_from_parser.version:
-        print("yaplmc", __version__)
-        exit()
+        self.code_mode = codemode.CodeMode(self)
+        self.run_mode = runmode.RunMode(self)
 
+        self.code_mode.grid(row=0, column=0, sticky=tkinter.NE + tkinter.SW)
+        self.code_mode.do_bindings()
+        self.code_mode.focus_set()
+        self.update_menu(self.code_mode.menus)
+
+    def update_menu(self, menus):
+        end = self.menu.index(tkinter.END)
+        for opts in menus:
+            self.menu.add_cascade(**opts)
+        self.menu.delete(0, end)
+
+    def runmode(self, *discard):
+        self.code_mode.grid_forget()
+        self.code_mode.do_unbindings()
+
+        self.run_mode.grid(row=0, column=0, sticky=tkinter.NE + tkinter.SW)
+        self.run_mode.do_bindings()
+        self.run_mode.focus_set()
+        self.update_menu(self.run_mode.menus)
+
+        ce = self.code_mode.current_codeeditor()
+        self.run_mode.set_code(ce.assembler, ce.fname)
+        self.run_mode.set_breakpoints(self.code_mode.current_codeeditor().breakpoints)
+        self.run_mode.reset()
+
+    def codemode(self, *discard):
+        self.run_mode.grid_forget()
+        self.run_mode.do_unbindings()
+
+        self.code_mode.grid(row=0, column=0, sticky=tkinter.NE + tkinter.SW)
+        self.code_mode.do_bindings()
+        self.code_mode.focus_set()
+        ce = self.code_mode.current_codeeditor()
+        ce.breakpoints = self.run_mode.code_editor.breakpoints
+        ce.update_sidebars()
+        self.update_menu(self.code_mode.menus)
+
+
+def main_gui():
+    t = GUIManager()
+    t.mainloop()
+    return 0
+
+
+def main_cli():
     if args_from_parser.file:
         code = open(args_from_parser.file).read()
     else:
@@ -70,7 +106,7 @@ under certain conditions. Type `yaplmc --licence` for details.
         print("\n".join(i.show(code.splitlines()) for i in assem.problems()))
     if assem.in_error:
         print("Assembly failed")
-        exit(1)
+        return 1
     print("Assembly succeeded")
     machine_code, code_length = (assem.machine_code,
                                  assem.machine_code_length)
@@ -94,3 +130,43 @@ under certain conditions. Type `yaplmc --licence` for details.
             run.give_input(int(input("<<< ")))
         if args_from_parser.debug >= 1:
             print(run.hint)
+    return 0
+
+
+if __name__ == "__main__":
+    import argparse
+
+    arg_parser = argparse.ArgumentParser(description=__doc__.split("\n")[1],
+                                         epilog="""
+yaplmc Copyright (C) 2014  Matthew Joyce
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions. Type `yaplmc --licence` for details.
+                                         """.strip())
+
+    arg_parser.add_argument("-f", "--file", help="lmc file",
+                            default=None)
+    arg_parser.add_argument("-l", "--licence", help="display licence",
+                            action="store_true")
+    arg_parser.add_argument("-V", "--version", help="display version",
+                            action="store_true")
+
+    cli_group = arg_parser.add_argument_group("CLI options (all options only active when -c or --cli used)")
+    cli_group.add_argument("-c", "--cli", help="use CLI mode",
+                            action="store_true")
+    cli_group.add_argument("-d", "--debug", help="debug level"
+                           " (repeat for more info, 3 is the max)",
+                           action="count", default=0)
+
+    args_from_parser = arg_parser.parse_args()
+
+    if args_from_parser.licence:
+        print(__doc__.strip())
+        exit()
+    elif args_from_parser.version:
+        print("yaplmc", __version__)
+        exit()
+    if args_from_parser.cli:
+        exit(main_cli())
+    else:
+        exit(main_gui())
