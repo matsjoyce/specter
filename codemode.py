828ebf00ca5141aacd42d49dfcafbc12d3b77c70
Matthew Joyce <matsjoyce@gmail.com>
2015-03-17 13:22:50 +0000

    Add F5 shortcut for assemble

--- codemode.py
+++ codemode.py
@@ -111,10 +111,12 @@ class CodeMode(tkinter.Frame):
     def do_bindings(self):
         self.bind_all("<Control-s>", self.save_current)
         self.bind_all("<Control-S>", self.saveas_current)
+        self.bind_all("<F5>", self.assemble)
 
     def do_unbindings(self):
         self.unbind_all("<Control-s>")
         self.unbind_all("<Control-S>")
+        self.unbind_all("<F5>")
 
     def current_codeeditor(self):
         if self.tabber.tabs():
